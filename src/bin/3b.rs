d75c4c1805b06a01fcc422e742c3830df3d91ebd
Jonathan Nilsson <jonathan@voysys.se>
2021-12-03 23:15:26 +0100

    Some cleanup

--- src/bin/3b.rs
+++ src/bin/3b.rs
@@ -1,5 +1,3 @@
-use core::num;
-
 use aoc2021::get_input;
 
 fn count_bit_digits(bit_count: usize, numbers: &[u32]) -> (Vec<u32>, Vec<u32>) {
@@ -19,6 +17,38 @@ fn count_bit_digits(bit_count: usize, numbers: &[u32]) -> (Vec<u32>, Vec<u32>) {
     (one_count, zero_count)
 }
 
+fn search_for_pattern(
+    bit_count: usize,
+    numbers: &[u32],
+    selector: impl Fn(u32, u32) -> bool,
+) -> u32 {
+    let mut remaining_numbers = numbers.to_vec();
+
+    for bit in 0..bit_count {
+        let (one_count, zero_count) = count_bit_digits(bit_count, remaining_numbers.as_slice());
+
+        let desired_value = if selector(one_count[bit], zero_count[bit]) {
+            1
+        } else {
+            0
+        };
+
+        let bit_pos = bit_count - 1 - bit;
+
+        remaining_numbers = remaining_numbers
+            .iter()
+            .copied()
+            .filter(|num| ((*num & (1 << bit_pos)) >> bit_pos) == desired_value)
+            .collect::<Vec<_>>();
+
+        if remaining_numbers.len() == 1 {
+            return *remaining_numbers.first().unwrap();
+        }
+    }
+
+    panic!("Didn't find value");
+}
+
 fn solve(input: &str) -> u32 {
     let mut bit_count = 0;
 
@@ -29,58 +59,13 @@ fn solve(input: &str) -> u32 {
         numbers.push(u32::from_str_radix(line, 2).unwrap());
     }
 
-    let mut oxygen_generator_rating = 0;
-    let mut remaining_oxygen = numbers.clone();
-
-    for bit in 0..bit_count {
-        let (one_count, zero_count) = count_bit_digits(bit_count, remaining_oxygen.as_slice());
-
-        let desired_value = if dbg!(one_count[bit]) >= dbg!(zero_count[bit]) {
-            1
-        } else {
-            0
-        };
-
-        let bit_pos = bit_count - 1 - bit;
-
-        remaining_oxygen = remaining_oxygen
-            .iter()
-            .copied()
-            .filter(|num| ((*num & (1 << bit_pos)) >> bit_pos) == desired_value)
-            .collect::<Vec<_>>();
-
-        if remaining_oxygen.len() == 1 {
-            oxygen_generator_rating = *remaining_oxygen.first().unwrap();
-            break;
-        }
-    }
-
-    let mut co2_scrubber_rating = 0;
-    let mut remaining_co2_scrubber = numbers.clone();
-
-    for bit in 0..bit_count {
-        let (one_count, zero_count) =
-            count_bit_digits(bit_count, remaining_co2_scrubber.as_slice());
-
-        let desired_value = if dbg!(one_count[bit]) < dbg!(zero_count[bit]) {
-            1
-        } else {
-            0
-        };
-
-        let bit_pos = bit_count - 1 - bit;
-
-        remaining_co2_scrubber = remaining_co2_scrubber
-            .iter()
-            .copied()
-            .filter(|num| ((*num & (1 << bit_pos)) >> bit_pos) == desired_value)
-            .collect::<Vec<_>>();
-
-        if remaining_co2_scrubber.len() == 1 {
-            co2_scrubber_rating = *remaining_co2_scrubber.first().unwrap();
-            break;
-        }
-    }
+    let oxygen_generator_rating =
+        search_for_pattern(bit_count, &numbers, |one_count, zero_count| {
+            one_count >= zero_count
+        });
+    let co2_scrubber_rating = search_for_pattern(bit_count, &numbers, |one_count, zero_count| {
+        one_count < zero_count
+    });
 
     oxygen_generator_rating * co2_scrubber_rating
 }
