c841d0c6b88f305dacad645c0a07229d933e669b
Jonathan Nilsson <jonathan@voysys.se>
2021-12-10 22:51:36 +0100

    10b

---
+++ src/bin/10b.rs
@@ -0,0 +1,90 @@
+use aoc2021::get_input;
+
+fn calc_score(c: char) -> i64 {
+    match c {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        _ => panic!("Invalid char"),
+    }
+}
+
+fn get_matchin(c: char) -> char {
+    match c {
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+        '>' => '<',
+        _ => panic!("Invalid char"),
+    }
+}
+
+fn get_reverse(c: char) -> char {
+    match c {
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
+        _ => panic!("Invalid char"),
+    }
+}
+
+fn solve(input: &str) -> i64 {
+    let mut line_scores = Vec::new();
+
+    'next_line: for line in input.lines() {
+        let mut stack = Vec::new();
+
+        for c in line.chars() {
+            match c {
+                '(' | '[' | '{' | '<' => stack.push(c),
+                ')' | ']' | '}' | '>' => {
+                    let last = stack.pop().unwrap();
+
+                    if last != get_matchin(c) {
+                        continue 'next_line;
+                    }
+                }
+                _ => panic!("Invalid input"),
+            }
+        }
+
+        let mut line_score = 0;
+
+        for compleating_c in stack.iter().rev() {
+            line_score = line_score * 5 + calc_score(get_reverse(*compleating_c));
+        }
+
+        line_scores.push(line_score);
+    }
+
+    line_scores.sort_unstable();
+
+    line_scores[line_scores.len() / 2]
+}
+
+fn main() {
+    let input = get_input();
+    let res = solve(&input);
+    println!("{}", res);
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        let input = "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+
+        assert_eq!(crate::solve(input), 288957)
+    }
+}
