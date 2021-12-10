b58f2104f4bc6f1f79c39dd8890a8125b3dfff83
Jonathan Nilsson <jonathan@voysys.se>
2021-12-10 22:40:50 +0100

    Day 10a

---
+++ src/bin/10a.rs
@@ -0,0 +1,71 @@
+use aoc2021::get_input;
+
+fn calc_score(c: char) -> i32 {
+    match c {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        _ => panic!("Invalid char"),
+    }
+}
+
+fn get_matchin(c: char) -> char {
+    match c {
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+        '>' => '<',
+        _ => panic!("Invalid char"),
+    }
+}
+
+fn solve(input: &str) -> i32 {
+    let mut score = 0;
+
+    for line in input.lines() {
+        let mut stack = Vec::new();
+
+        for c in line.chars() {
+            match c {
+                '(' | '[' | '{' | '<' => stack.push(c),
+                ')' | ']' | '}' | '>' => {
+                    let last = stack.pop().unwrap();
+
+                    if last != get_matchin(c) {
+                        score += calc_score(c);
+                        break;
+                    }
+                }
+                _ => panic!("Invalid input"),
+            }
+        }
+    }
+
+    score
+}
+
+fn main() {
+    let input = get_input();
+    let res = solve(&input);
+    println!("{}", res);
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        let input = "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+
+        assert_eq!(crate::solve(input), 26397)
+    }
+}
