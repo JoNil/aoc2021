becb656742031bda5cc8ded82be49e8df03c020e
Jonathan Nilsson <jonathan@voysys.se>
2021-12-16 17:55:06 +0100

    Start 16

---
+++ src/bin/16a.rs
@@ -0,0 +1,42 @@
+use aoc2021::get_input;
+
+fn solve(input: &str) -> i32 {
+    0
+}
+
+fn main() {
+    let input = get_input();
+    let start = std::time::Instant::now();
+    let res = solve(&input);
+    let end = start.elapsed();
+    println!(
+        "Day {} ({:?}): {}",
+        aoc2021::get_program_name(),
+        end.as_micros(),
+        res
+    );
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        let input = "0 = 0000
+1 = 0001
+2 = 0010
+3 = 0011
+4 = 0100
+5 = 0101
+6 = 0110
+7 = 0111
+8 = 1000
+9 = 1001
+A = 1010
+B = 1011
+C = 1100
+D = 1101
+E = 1110
+F = 1111";
+        assert_eq!(crate::solve(input), 31)
+    }
+}
