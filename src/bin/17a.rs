3644261cdfcab8a64b5785c512337ddbfd46b3b7
Jonathan Nilsson <jonathan@voysys.se>
2021-12-17 18:36:23 +0100

    Day 17a

--- src/bin/17a.rs
+++ src/bin/17a.rs
@@ -12,81 +12,47 @@ struct TargetArea {
     end: IVec2,
 }
 
-pub fn print_map(map: &HashMap<IVec2, impl fmt::Display>) {
-    let min_x = map.keys().map(|v| v.x).min().unwrap();
-    let min_y = map.keys().map(|v| v.y).min().unwrap();
-
-    let max_x = map.keys().map(|v| v.x).max().unwrap();
-    let max_y = map.keys().map(|v| v.y).max().unwrap();
-
-    for y in (min_y..=max_y).rev() {
-        for x in min_x..=max_x {
-            let hit = map
-                .get(&ivec2(x, y))
-                .map(|count| format!("{}", count))
-                .unwrap_or_else(|| ".".to_string());
-
-            print!("{}", hit);
-        }
-        println!();
-    }
-}
-
 fn solve(input: &str) -> i32 {
     let target = input.trim().parse::<TargetArea>().unwrap();
 
-    let mut map = HashMap::new();
-
-    for y in target.start.y..=target.end.y {
-        for x in target.start.x..=target.end.x {
-            map.insert(ivec2(x, y), 'T');
-        }
-    }
-
     let mut max_y = 0;
 
-    let mut pos = ivec2(0, 0);
-    let mut velocity = ivec2(6, 4);
+    for dx in 1..target.end.x {
+        for dy in target.start.y..1000 {
+            let mut pos = ivec2(0, 0);
+            let mut velocity = ivec2(dx, dy);
+            let mut local_max_y = 0;
 
-    for _ in 0.. {
-        pos += velocity;
-        velocity.x = (velocity.x - 1).max(0);
-        velocity.y -= 1;
+            for _ in 0.. {
+                pos += velocity;
+                velocity.x = (velocity.x - 1).max(0);
+                velocity.y -= 1;
 
-        if pos.y > max_y {
-            max_y = pos.y;
+                if pos.y > local_max_y {
+                    local_max_y = pos.y;
+                }
+
+                // Collided
+                if pos.x <= target.end.x
+                    && pos.x >= target.start.x
+                    && pos.y >= target.start.y
+                    && pos.y <= target.end.y
+                {
+                    if local_max_y > max_y {
+                        max_y = local_max_y;
+                    }
+                    break;
+                }
+
+                // Stop if we are past
+                if pos.x > target.end.x || pos.y < target.start.y {
+                    break;
+                }
+            }
         }
-
-        // Collided
-        if pos.x <= target.end.x
-            && pos.x >= target.start.x
-            && pos.y >= target.start.y
-            && pos.y <= target.end.y
-        {
-            map.insert(pos, 'B');
-            println!("Collided!");
-            break;
-        }
-
-        // Stop if we are past
-        if pos.x > target.end.x || pos.y < target.start.y {
-            dbg!(pos);
-            dbg!(target.end);
-            break;
-        }
-
-        map.insert(pos, '#');
     }
 
-    map.insert(ivec2(0, 0), 'S');
-
-    dbg!(max_y);
-
-    print_map(&map);
-
-    //dbg!(&map);
-
-    0
+    max_y
 }
 
 fn main() {
