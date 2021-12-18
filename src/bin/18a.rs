0bf9fc879e8bee14b145642f4d0ab37b19fa0371
Jonathan Nilsson <jonathan@voysys.se>
2021-12-18 16:00:02 +0100

    Almost exploded

--- src/bin/18a.rs
+++ src/bin/18a.rs
@@ -1,31 +1,66 @@
-use std::fmt::Debug;
-
 use aoc2021::get_input;
+use std::fmt::Debug;
+use Number::{Literal, Pair};
 
 enum Number {
-    Literal {
-        value: i32,
-    },
-    Pair {
-        left: Box<Number>,
-        right: Box<Number>,
-    },
+    Literal(i32),
+    Pair(Box<Number>, Box<Number>),
 }
 
 impl Number {
-    fn add(self, rhs: Number) -> Number {
-        Number::Pair {
-            left: Box::new(self),
-            right: Box::new(rhs),
+    fn is_regular_pair(&self) -> Option<(i32, i32)> {
+        match self {
+            Pair(left, right) => match (&**left, &**right) {
+                (Literal(lhs), Literal(rhs)) => Some((*lhs, *rhs)),
+                _ => None,
+            },
+            _ => None,
         }
+    }
+
+    fn explode(self, depth: i32) -> (Option<(i32, i32)>, Number) {
+        if depth >= 4 {
+            if let Some((lhs, rhs)) = self.is_regular_pair() {
+                return (Some((lhs, rhs)), Literal(0));
+            }
+        }
+
+        match self {
+            Literal(val) => (None, Literal(val)),
+            Pair(lhs, rhs) => {
+                let lhs = match lhs.explode(depth + 1) {
+                    (Some((left, right)), num) => {
+                        return (Some((left, right)), Pair(Box::new(num), rhs));
+                    }
+                    (None, lhs) => lhs,
+                };
+
+                let rhs = match rhs.explode(depth + 1) {
+                    (Some((left, right)), num) => {
+                        return (Some((left, right)), Pair(Box::new(lhs), Box::new(num)));
+                    }
+                    (None, rhs) => rhs,
+                };
+
+                (None, Pair(Box::new(lhs), Box::new(rhs)))
+            }
+        }
+    }
+
+    fn reduce(self) -> Number {
+        self.explode(1).1
+    }
+
+    fn add(self, rhs: Number) -> Number {
+        Pair(Box::new(self), Box::new(rhs)).reduce()
     }
 }
 
 impl Debug for Number {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Number::Literal { value } => write!(f, "{:?}", value),
-            Number::Pair { left, right } => write!(f, "[{:?},{:?}]", left, right),
+            Literal(value) => write!(f, "{:?}", value),
+            Pair(left, right) => write!(f, "[{:?},{:?}]", left, right),
         }
     }
 }
@@ -62,18 +97,13 @@ impl<'a> Parser<'a> {
             assert!(self.take_byte()? == b',');
 
             let right = self.take_number()?;
-            let res = Number::Pair {
-                left: Box::new(left),
-                right: Box::new(right),
-            };
+            let res = Number::Pair(Box::new(left), Box::new(right));
 
             assert!(self.take_byte()? == b']');
 
             Some(res)
         } else {
-            Some(Number::Literal {
-                value: (byte as char).to_digit(10).unwrap() as i32,
-            })
+            Some(Number::Literal((byte as char).to_digit(10).unwrap() as i32))
         }
     }
 }
@@ -123,5 +153,21 @@ mod test {
 [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
 [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]";
         assert_eq!(crate::solve(input), 4140);
+    }
+
+    #[test]
+    fn test_explode() {
+        use crate::Parser;
+
+        let data = [
+            ("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"),
+            ("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"),
+            ("[[[[[9,8],1],2],3],4]", "[[6,[5,[4,[3,2]]]],1]"),
+        ];
+
+        for (input, output) in data {
+            let mut number = Parser::new(input).take_number().unwrap().reduce();
+            assert_eq!(format!("{:?}", number), output);
+        }
     }
 }
